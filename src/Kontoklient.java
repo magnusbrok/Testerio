bb237b0218e7ba1f868f66655b5745f704e79e36
magnusbrok <magnus.brok@db-data.dk>
2020-03-11 19:45:25 +0100

    3

--- src/Kontoklient.java
+++ src/Kontoklient.java
@@ -6,7 +6,7 @@ public class Kontoklient
 	public static void main(String[] arg) throws Exception
 	{
 		Thread.sleep(500);
-		KontoI k =(KontoI) Naming.lookup("rmi://s185037@dist.saluton.dk/kontotjeneste");
+		KontoI k =(KontoI) Naming.lookup("rmi://dist.saluton.dk:1099/kontotjeneste");
     k.overførsel(100);
     k.overførsel(50);
 		System.out.println( "Saldo er: "+ k.saldo() );
