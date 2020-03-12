282aaf56fa67452d971a986732bc2fcd41a6eef7
magnusbrok <magnus.brok@db-data.dk>
2020-03-12 20:31:04 +0100

    new jar

--- src/Kontoserver.java
+++ src/Kontoserver.java
@@ -5,16 +5,18 @@ public class Kontoserver
 	private static int PORT = 1099;
 	public static void main(String[] arg) throws Exception
 	{
+		System.setProperty("java.rmi.server.hostname", "dist.saluton.dk");
+
+
 		// Enten: Kør programmet 'rmiregistry' fra mappen med .class-filerne, eller:
 		java.rmi.registry.LocateRegistry.createRegistry(PORT); // start i server-JVM
 		KontoImpl k = new KontoImpl();
 
 		//Naming.rebind("rmi://localhost:"+PORT+"/kontotjeneste", k);
 
-		System.setProperty("java.rmi.server.hostname", "dist.saluton.dk");
 		Naming.rebind("rmi://dist.saluton.dk:"+PORT+"/kontotjeneste", k);
 
-		System.out.println("Kontotjeneste registreret magn.");
+		System.out.println("Kontotjeneste registreret magn. nu med setPropertyOverst");
 		//k.overførsel(117);
 		//k.overførsel(1023);
 
